52e1c50496396d419dcb5d25ffea9d1603c7786b
stefan.najdovski <stefan.najdovski@aol.com>
2025-07-19 18:38:47 +0200

    Parsing HTTP Requsts

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 #define HEADER "HTTP/1.1 200 OK"
 
-char *header = HEADER;
+//forward declaration
+char to_path(char* req);
 
 int main(){
 
+char *header = HEADER;
+char *path = to_path(req);
+
 write(1,header,strlen(header));
+printf("Memory Address:%zu\n",header);
+printf("Header:%s",header);
+
+
+return EXIT_SUCCESS;
+}
+
+
+
+char to_path(char* req){
 
 }
